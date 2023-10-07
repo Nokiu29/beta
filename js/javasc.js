c2f62d158e6b78d097496d9b2c1ea4a76801fc5e
Nokiu29 <147274175+Nokiu29@users.noreply.github.com>
2023-10-07 21:46:09 +0300

    Update javasc.js

--- js/javasc.js
+++ js/javasc.js
@@ -12,3 +12,25 @@ closeButton.addEventListener('click', function () {
     mobileMenu.style.left = '-250px'; // Закрываем боковую панель
     body.classList.remove('menu-open'); // Разрешаем прокрутку страницы
 });
+// Выбираем элемент заголовка
+const header = document.querySelector('.transparent-header');
+
+let lastScrollPosition = 0;
+
+// Обработчик события прокрутки
+window.addEventListener('scroll', function () {
+    // Текущая позиция прокрутки страницы
+    const currentScrollPosition = window.scrollY;
+
+    // Если текущая позиция прокрутки больше предыдущей, это значит, что страница прокручивается вниз
+    if (currentScrollPosition > lastScrollPosition) {
+        // Скрываем заголовок
+        header.classList.add('hide');
+    } else {
+        // Показываем заголовок
+        header.classList.remove('hide');
+    }
+
+    // Обновляем позицию прокрутки
+    lastScrollPosition = currentScrollPosition;
+});
